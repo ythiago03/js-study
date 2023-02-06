0add5938fe2c06b884763b0db7e529e87d91a822
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-02-06 18:31:48 -0300

    Training variables

---
+++ treinando variaveis/main.js
@@ -0,0 +1,3 @@
+const  nome = 'Thiago'// const permite que mude o valor, tem escopo de bloco e não sofre hoisting
+let idade = 20// le permite que mude o valor, tem escopo de bloco e não sofre hoisting
+var animeFavorito = 'Mushoku tensei'//var permite que mude o valor, tem escopo vazado e sofre hoisting
