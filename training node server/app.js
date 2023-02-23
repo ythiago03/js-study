4ff8d28e62fc025c3a5502710518ef38867deb98
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-02-23 18:56:18 -0300

    Adding PUT method and DELETE method

--- training node server/app.js
+++ training node server/app.js
@@ -33,6 +33,35 @@ app.get('/products', (req, res) => {
     return res.json(products);
 })
 
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const product = products.find(product => product.id === id)
+    return res.json(product)
+})
+
+app.put('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const {name, price} = req.body;
+
+    const productIndex = products.findIndex(product => product.id === id)
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price
+    }
+
+    return res.json({message: 'Produto alterado com sucesso'})
+})
+
+app.delete('/products/:id', (req, res) => {
+    const { id } = req.params;
+
+    const productIndex = products.findIndex(product => product.id === id)
+    products.splice(productIndex, 1)
+
+    return res.json({message: 'Produto deletado com sucesso'})
+})
+
 app.listen('4002', () => {
     console.log('Server rodando na porta 4002');
 })
